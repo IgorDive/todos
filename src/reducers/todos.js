6d4130277f49dcac63a4950ad43481a7d5edc652
IgorDive <ihar.kazlovich@gmail.com>
2018-11-27 17:41:06 +0300

    change state form

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,24 +1,37 @@
+import { combineReducers } from 'redux';
 import todo from './todo'
 
-const todos = (state = [], action) => {
+const byId = (state = {}, action) => {
     switch (action.type) {
-        case 'ADD_TODO': return [...state, todo(undefined, action),];
-        case 'TOGGLE_TODO': return state.map(t => todo(t, action));
+        case 'ADD_TODO': 
+        case 'TOGGLE_TODO': return {...state, [action.id]: todo(state[action.id], action)};
         default: return state;
     }
 };
  
+const allIds = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO': return [...state, action.id];
+        default: return state;
+    }
+};
+
+const todos = combineReducers({
+    byId,
+    allIds
+});
+
 export default todos;
 
+const getAllTodos = (state) => state.allIds.map( id => state.byId[id]); 
+
 export const getVisibleTodos = (state, filter) => {
-    let visibleTodos = state.filter( todo => {
-        switch(filter) {
-            case 'all': return state;
-            case 'active': return !todo.completed;
-            case 'completed': return todo.completed;
-            default: throw new Error(`Unknown filter: ${filter}`);
-        }
-    } );
+    let allTodos = getAllTodos(state);
     
-    return visibleTodos; 
-}
+    switch(filter) {
+        case 'all': return allTodos;
+        case 'active': return allTodos.filter( todo => !todo.completed );
+        case 'completed': return allTodos.filter( todo => todo.completed );
+        default: throw new Error(`Unknown filter: ${filter}`);
+    };
+};
